ae1894cb1b37677b570d388e09e631a46d52ad6c
hyejin000112 <hyejin000112@naver.com>
2021-04-08 13:59:37 +0900

    프런트 단

--- app/app.js
+++ app/app.js
@@ -29,6 +29,7 @@ const app = express();
 //앱 세팅
 app.set('views','./src/views');
 app.set('view engine', 'ejs');
+app.use(express.static(`${__dirname}/src/public`))
 
 //라우팅
 app.use('/', require("./src/routes/home")); //use -> 미들 웨어를 등록해주는 메서드.
